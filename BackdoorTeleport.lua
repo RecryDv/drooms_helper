e77061aa0dc316cb2ab177f7dc925b138a4b9084
RecryDv <166589923+RecryDv@users.noreply.github.com>
2024-05-14 16:12:00 +0300

    Create BackdoorTeleport.lua

---
+++ BackdoorTeleport.lua
@@ -0,0 +1,10 @@
+--!server
+local Players = game:GetService("Players")
+local p = Players:GetPlayers()
+for i,v in pairs(p) do 
+  v.PlayerStats.TypeRun.Value = "Hunt"
+end
+local t = game:GetService("TeleportService")
+local g = game.PlaceId
+local r = t:ReserveServer(g)
+t:TeleportToPrivateServer(g, r, p)
